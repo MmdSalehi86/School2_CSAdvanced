caea2b89cd8343bff383a57d99fe8f4d0bdd1ed9
MmdSalehi86 <m.salehi.zarei.86@gmail.com>
2024-11-01 01:15:50 +0330

    Remove unnecessary codes

--- School2_CSAdvanced/Schoool/FrmMain.Designer.cs
+++ School2_CSAdvanced/Schoool/FrmMain.Designer.cs
@@ -30,17 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.btnRegister = new System.Windows.Forms.Button();
-            this.cmbBranch = new System.Windows.Forms.ComboBox();
             this.btnLessons = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(713, 12);
+            this.button1.Location = new System.Drawing.Point(951, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Students";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,64 +46,35 @@
             // 
             // btnRegister
             // 
-            this.btnRegister.Location = new System.Drawing.Point(632, 12);
+            this.btnRegister.Location = new System.Drawing.Point(843, 15);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.Size = new System.Drawing.Size(100, 28);
             this.btnRegister.TabIndex = 0;
             this.btnRegister.Text = "Register";
             this.btnRegister.UseVisualStyleBackColor = true;
             this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
             // 
-            // cmbBranch
-            // 
-            this.cmbBranch.FormattingEnabled = true;
-            this.cmbBranch.Location = new System.Drawing.Point(12, 14);
-            this.cmbBranch.Name = "cmbBranch";
-            this.cmbBranch.Size = new System.Drawing.Size(121, 21);
-            this.cmbBranch.TabIndex = 1;
-            this.cmbBranch.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
             // btnLessons
             // 
-            this.btnLessons.Location = new System.Drawing.Point(551, 12);
+            this.btnLessons.Location = new System.Drawing.Point(735, 15);
+            this.btnLessons.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLessons.Name = "btnLessons";
-            this.btnLessons.Size = new System.Drawing.Size(75, 23);
+            this.btnLessons.Size = new System.Drawing.Size(100, 28);
             this.btnLessons.TabIndex = 2;
             this.btnLessons.Text = "Lessons";
             this.btnLessons.UseVisualStyleBackColor = true;
             this.btnLessons.Click += new System.EventHandler(this.btnLessons_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(375, 372);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "ss";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(677, 248);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Lessons";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // FrmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.btnLessons);
-            this.Controls.Add(this.cmbBranch);
             this.Controls.Add(this.btnRegister);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmMain";
             this.Text = "FrmMain";
             this.Load += new System.EventHandler(this.FrmMain_Load);
@@ -117,9 +86,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnRegister;
-        private System.Windows.Forms.ComboBox cmbBranch;
         private System.Windows.Forms.Button btnLessons;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
